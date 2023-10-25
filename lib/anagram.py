731c0d0ac68293e94b349a9370b2afcae1275afb
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-10-25 17:06:55 -0400

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word  = word
+
+    def match(self, word_list):
+        result = []
+        for w in word_list:
+            if sorted(list(self.word)) == sorted(list(w)):
+                result.append(w)
+        return result
